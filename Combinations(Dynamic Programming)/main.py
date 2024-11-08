174f2583c576946cd946f9257995cbf1975d9f07
Erfan Rajati Haghi <main.e.rajati@gmail.com>
2024-11-08 09:53:34 +0330

    Add non recursive approach for combinations

--- Combinations(Dynamic Programming)/main.py
+++ Combinations(Dynamic Programming)/main.py
@@ -22,6 +22,23 @@ def combinations(n, k, memo=None):
     
     return result
 
+
+# Non Recursive
+def combinations(n, k):
+    dp = [[0] * (k + 1) for _ in range(n + 1)]
+    
+    for i in range(n + 1):
+        dp[i][0] = 1  # C(i, 0) = 1
+        if i <= k:
+            dp[i][i] = 1  # C(i, i) = 1
+    
+    for i in range(1, n + 1):
+        for j in range(1, min(i, k) + 1):  # Only compute up to min(i, k) to stay within bounds
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+    
+    return dp[n][k]
+
+
 def main():
     n, k = [int(i) for i in input().split()]
     print(combinations(n, k))
@@ -29,3 +46,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
